d704faa8dd7acc20c4983276a8f17fd76c69ae60
David Domkář <dave.domkar@gmail.com>
2020-07-18 03:11:49 +0200

    feat: improve default layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template lang="pug">
-  ntt-logo
+  .page
+    ntt-logo
+    h1.mt-8 Hello Fellow Humans
 </template>
+
+<style lang="sass" scoped>
+.page
+  @apply flex flex-col justify-center items-center h-screen
+</style>
 
 <script lang="ts">
 import { defineComponent } from 'nuxt-composition-api';
